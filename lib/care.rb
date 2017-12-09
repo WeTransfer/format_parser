f3adb72dd13666c9686b2f0925568a9e762e68b4
Julik Tarkhanov <me@julik.nl>
2017-12-10 00:29:50 +0100

    Patch up the tests

--- lib/care.rb
+++ lib/care.rb
@@ -1,3 +1,8 @@
+# Care (Caching Reader) makes it more efficient to feed a
+# possibly remote IO to parsers that tend to read (and skip)
+# in very small increments. This way, with a remote source that
+# is only available via HTTP, for example, we can have less
+# fetches and perform them in larger chunks
 class Care
   class IOWrapper
     def initialize(io, cache)
@@ -10,52 +15,58 @@ class Care
     end
 
     def read(n_bytes)
-      @cache.byteslice(@io, @pos, n_bytes).tap {|read| @pos += read.to_s.bytesize }
+      read = @cache.byteslice(@io, @pos, n_bytes)
+      return nil unless read && !read.empty?
+      @pos += read.bytesize
+      read
     end
   end
 
-  def initialize(page_size)
-    @page_size = page_size.to_i
-    raise ArgumentError, "The page size must be a positive Integer" unless @page_size > 0
-    @pages = {}
-    @lowest_known_empty_page = nil
-  end
-
-  def byteslice(io, at, n_bytes)
-    first_page = at / @page_size
-    last_page = (at + n_bytes) / @page_size
-    local_offset_on_first_page = at % @page_size
-
-    relevant_pages = (first_page..last_page).map{|i| hydrate_page(io, i) }
-
-    slab = relevant_pages.join
-    slice = slab.byteslice(local_offset_on_first_page, n_bytes)
-
-    if slice && !slice.empty?
-      slice
-    else
-      nil
-    end
-  end
-
-  def hydrate_page(io, page_i)
-    # Avoid trying to read the page if we know there is no content to fill it
-    # in the underlying IO
-    if @lowest_known_empty_page && page_i >= @lowest_known_empty_page
-      return nil
+  # Stores slabs from a given source in contiguous pages.
+  class Cache
+    def initialize(page_size)
+      @page_size = page_size.to_i
+      raise ArgumentError, "The page size must be a positive Integer" unless @page_size > 0
+      @pages = {}
+      @lowest_known_empty_page = nil
     end
 
-    @pages[page_i] ||= begin
-      io.seek(page_i * @page_size)
-      read_result = io.read(@page_size)
+    def byteslice(io, at, n_bytes)
+      first_page = at / @page_size
+      last_page = (at + n_bytes) / @page_size
+      local_offset_on_first_page = at % @page_size
 
-      if read_result.nil?
-        if @lowest_known_empty_page.nil? || @lowest_known_empty_page > page_i
-          @lowest_known_empty_page = page_i
-        end
+      relevant_pages = (first_page..last_page).map{|i| hydrate_page(io, i) }
+
+      slab = relevant_pages.join
+      slice = slab.byteslice(local_offset_on_first_page, n_bytes)
+
+      if slice && !slice.empty?
+        slice
+      else
+        nil
+      end
+    end
+
+    def hydrate_page(io, page_i)
+      # Avoid trying to read the page if we know there is no content to fill it
+      # in the underlying IO
+      if @lowest_known_empty_page && page_i >= @lowest_known_empty_page
+        return nil
       end
 
-      read_result
+      @pages[page_i] ||= begin
+        io.seek(page_i * @page_size)
+        read_result = io.read(@page_size)
+
+        if read_result.nil?
+          if @lowest_known_empty_page.nil? || @lowest_known_empty_page > page_i
+            @lowest_known_empty_page = page_i
+          end
+        end
+
+        read_result
+      end
     end
   end
 end
--- spec/care_spec.rb
+++ spec/care_spec.rb
@@ -1,54 +1,69 @@
 require 'spec_helper'
 
 describe Care do
-  let(:source) { StringIO.new("Hello there, this is our little caching reader") }
+  describe Care::Cache do
+    let(:source) { StringIO.new("Hello there, this is our little caching reader") }
 
-  it 'performs correct reads at various offsets' do
-    cache = Care.new(3)
-    expect(cache.byteslice(source, 0, 3)).to eq("Hel")
-    expect(cache.byteslice(source, 0, 7)).to eq("Hello t")
-    expect(cache.byteslice(source, 1, 7)).to eq("ello th")
-    expect(cache.byteslice(source, 11, 8)).to eq(", this i")
-    expect(cache.byteslice(source, 12, 12)).to eq(" this is our")
-    expect(cache.byteslice(source, 120, 12)).to be_nil
-  end
+    it 'performs correct reads at various offsets' do
+      cache = Care::Cache.new(3)
+      expect(cache.byteslice(source, 0, 3)).to eq("Hel")
+      expect(cache.byteslice(source, 0, 7)).to eq("Hello t")
+      expect(cache.byteslice(source, 1, 7)).to eq("ello th")
+      expect(cache.byteslice(source, 11, 8)).to eq(", this i")
+      expect(cache.byteslice(source, 12, 12)).to eq(" this is our")
+      expect(cache.byteslice(source, 120, 12)).to be_nil
+    end
 
-  it 'fits all the reads into one if the input fits into one page' do
-    expect(source).to receive(:read).at_most(2).times.and_call_original
+    it 'fits all the reads into one if the input fits into one page' do
+      expect(source).to receive(:read).at_most(2).times.and_call_original
 
-    cache = Care.new(source.size)
+      cache = Care::Cache.new(source.size)
 
-    cache.byteslice(source, 0, 3)
-    cache.byteslice(source, 11, 8)
-    cache.byteslice(source, 190, 12)
-    cache.byteslice(source, 290, 1)
-    cache.byteslice(source, 890, 1)
-  end
+      cache.byteslice(source, 0, 3)
+      cache.byteslice(source, 11, 8)
+      cache.byteslice(source, 190, 12)
+      cache.byteslice(source, 290, 1)
+      cache.byteslice(source, 890, 1)
+    end
 
-  it 'permits oversized reads' do
-    cache = Care.new(4)
-    expect(cache.byteslice(source, 0, 999)).to eq(source.string)
-  end
+    it 'permits oversized reads' do
+      cache = Care::Cache.new(4)
+      expect(cache.byteslice(source, 0, 999)).to eq(source.string)
+    end
 
-  it 'returns nil with an empty input' do
-    cache = Care.new(4)
-    expect(cache.byteslice(StringIO.new(''), 0, 1)).to be_nil
+    it 'returns nil with an empty input' do
+      cache = Care::Cache.new(4)
+      expect(cache.byteslice(StringIO.new(''), 0, 1)).to be_nil
+    end
   end
 
   describe Care::IOWrapper do
-    it 'forwards calls to read() to the Care' do
-      cache_double = double('Care')
+    it 'forwards calls to read() to the Care and adjusts internal offsets' do
+      fake_cache_class = Class.new do
+        attr_reader :recorded_calls
+        def byteslice(io, at, n_bytes)
+          @recorded_calls ||= []
+          @recorded_calls << [io, at, n_bytes]
+          # Pretend reads always succeed and return the requisite number of bytes
+          "x" * n_bytes
+        end
+      end
+
+      cache_double = fake_cache_class.new
       io_double = double('IO')
 
-      expect(cache_double).to receive(:byteslice).with(io_double, 0, 2).ordered
-      expect(cache_double).to receive(:byteslice).with(io_double, 2, 3).ordered
-      expect(cache_double).to receive(:byteslice).with(io_double, 11, 5).ordered
-
       subject = Care::IOWrapper.new(io_double, cache_double)
+
       subject.read(2)
       subject.read(3)
       subject.seek(11)
       subject.read(5)
+
+      expect(cache_double.recorded_calls).to be_kind_of(Array)
+      first, second, third = *cache_double.recorded_calls
+      expect(first).to eq([io_double,  0, 2])
+      expect(second).to eq([io_double, 2, 3])
+      expect(third).to eq([io_double,  11, 5])
     end
   end
 end
