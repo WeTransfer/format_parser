7f8c1dc380eaaf080298cbc54fec6f12e335810d
Noah Berman <noah@wetransfer.com>
2017-12-11 15:38:36 +0100

    Test all TIFF examples

--- spec/tiff_parser_spec.rb
+++ spec/tiff_parser_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe FormatParser::TIFFParser do
   describe 'is able to parse all the examples from FastImage' do
-    Dir.glob(__dir__ + '/fixtures/*.tiff').each do |tiff_path|
+    Dir.glob(__dir__ + '/fixtures/TIFF/*.tif').each do |tiff_path|
       it "is able to parse #{File.basename(tiff_path)}" do
         parsed = subject.information_from_io(File.open(tiff_path, 'rb'))
         expect(parsed).not_to be_nil
