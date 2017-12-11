f25c88a249c32ed3516c3fba069254d4fe01fe76
Julik Tarkhanov <me@julik.nl>
2017-12-11 19:59:49 +0100

    Maan my bit shifting is rusty

--- lib/parsers/gif_parser.rb
+++ lib/parsers/gif_parser.rb
@@ -11,9 +11,26 @@ class FormatParser::GIFParser
 
     w, h = safe_read(io, 4).unpack('vv')
     gct_byte, bgcolor_index, pixel_aspect_ratio = safe_read(io, 5).unpack('Cvv')
-    return FormatParser::FileInformation.new(width_px: w, height_px: h)
-    
+
     # and actually onwards for this:
     # http://www.matthewflickinger.com/lab/whatsinagif/bits_and_bytes.asp
+    
+    # Determine how big our color table is
+    has_gct = gct_byte[0] == 1
+    bytes_per_color = gct_byte >> 6
+    unpacked_radix = gct_byte & 0b00000111
+    num_colors = 2**(unpacked_radix + 1)
+    gct_table_size = num_colors*bytes_per_color
+
+    # If we have the global color table - skip over it
+    if has_gct
+      safe_read(io, gct_table_size)
+    end
+
+    # Now it gets interesting - potentially we are at the place where an
+    # application extension for the NETSCAPE2.0 block will occur
+    raise safe_read(io, 64).inspect
+
+    return FormatParser::FileInformation.new(width_px: w, height_px: h)
   end
 end
