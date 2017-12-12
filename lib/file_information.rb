8d141701f816fa673e051725d773d87cb3616dd2
Julik Tarkhanov <me@julik.nl>
2017-12-12 18:07:09 +0100

    Pull out the FileInformation struct into a class

---
+++ lib/file_information.rb
@@ -0,0 +1,13 @@
+class FormatParser::FileInformation
+
+  # Number of pixels horizontally in the pixel buffer
+  attr_accessor :width_px
+
+  # Number of pixels vertically in the pixel buffer
+  attr_accessor :height_px
+
+  # Only permits assignments via defined accessors
+  def initialize(**kwargs)
+    kwargs.map { |(k, v)| public_send("#{k}=", v) }
+  end
+end
