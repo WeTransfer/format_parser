7e188c6c90c0d743f281a03664beca6191accc76
Noah Berman <noah@wetransfer.com>
2017-12-13 10:28:39 +0100

    Convert attr setup to dry-types gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'dry-types'
+gem 'dry-struct'
+
 group :development do
   gem 'rspec'
   gem 'pry'
--- lib/file_information.rb
+++ lib/file_information.rb
@@ -1,33 +1,34 @@
+require 'dry-types'
+require 'dry-struct'
+
 module FormatParser
-  class FileInformation
+
+  module Types
+    include Dry::Types.module
+  end
+
+  class FileInformation < Dry::Struct
     # What kind of file is it?
-    attr_accessor :file_nature
+    attribute :file_nature, Types::Strict::String
 
     # What filetype was recognized? Will contain a non-ambiguous symbol
     # referring to the file format. The symbol can be used as a filename
     # extension safely
-    attr_accessor :file_type
+    attribute :file_type, Types::Strict::Symbol
 
     # Number of pixels horizontally in the pixel buffer
-    attr_accessor :width_px
+    attribute :width_px, Types::Strict::Int.optional
 
     # Number of pixels vertically in the pixel buffer
-    attr_accessor :height_px
+    attribute :height_px, Types::Strict::Int.optional
 
     # Whether the file has multiple frames (relevant for image files and video)
-    attr_accessor :has_multiple_frames
+    attribute :has_multiple_frames, Types::Strict::Bool.optional
 
-    # The angle by which the camera was rotated when taking the picture
-    # (affects display width and height)
-    attr_accessor :exif_orientation_angle
+    # Image orientation value from EXIF. Can be between 1-9.
+    # Some guidlines for using this number can be found here
+    # https://beradrian.wordpress.com/2008/11/14/rotate-exif-images/
+    attribute :exif_orientation, Types::Strict::Int.optional
 
-    # Only permits assignments via defined accessors
-    def initialize(**kwargs)
-      kwargs.map { |(k, v)| public_send("#{k}=", v) }
-    end
-
-    def self.image(**kwargs)
-      new(file_nature: :image, **kwargs)
-    end
   end
 end
