b32bdfcc5a5b033576d81b1f8dff6f0b6fa056da
Julik Tarkhanov <me@julik.nl>
2017-12-14 11:12:20 +0100

    Reintroduce methods

--- lib/file_information.rb
+++ lib/file_information.rb
@@ -1,59 +1,64 @@
-require 'dry-validation'
-
 module FormatParser
-
-  class ParsingError < StandardError; end
-
   class FileInformation
-    VALID_FILE_NATURES = [:image]
-    SUPPORTED_FILE_TYPES = [
-      :jpg, :gif, :png, :psd, :dpx, :tif
-    ]
-    # Will only allow the attributes we specify, but since not all filetypes
-    # will use all attributes, we can set them to a default value and it will
-    # ignore them.
+    # What kind of file is it?
+    attr_accessor :file_nature
 
-    def self.validate_and_return(**attributes)
-      schema = Dry::Validation.Schema do
-        required(:file_nature).filled(included_in?: VALID_FILE_NATURES)
-        required(:file_type).filled(included_in?: SUPPORTED_FILE_TYPES)
-        optional(:width_px).filled(:int?)
-        optional(:height_px).filled(:int?)
-        optional(:has_multiple_frames).filled(:bool?)
-      end
+    # What filetype was recognized? Will contain a non-ambiguous symbol
+    # referring to the file format. The symbol can be used as a filename
+    # extension safely
+    attr_accessor :file_type
 
+    # Number of pixels horizontally in the pixel buffer
+    attr_accessor :width_px
+
+    # Number of pixels vertically in the pixel buffer
+    attr_accessor :height_px
+
+    # Whether the file has multiple frames (relevant for image files and video)
+    attr_accessor :has_multiple_frames
+
+    # The angle by which the camera was rotated when taking the picture
+    # (affects display width and height)
+    attr_accessor :exif_orientation_angle
+
+    # Whether the image has transparency (or an alpha channel)
+    attr_accessor :has_transparency
+
+    # Basic information about the color mode
+    attr_accessor :color_mode
+
+    # If the file has animation or is video, this might
+    # indicate the number of frames. Some formats do not
+    # allow retrieving this value without parsing the entire
+    # file, so for GIF this might be nil even though it is
+    # animated. For a boolean check, `has_multiple_frames`
+    # might offer a better clue.
+    attr_accessor :num_animation_or_video_frames
+
+    # Only permits assignments via defined accessors
+    def initialize(**kwargs)
+      kwargs.map { |(k, v)| public_send("#{k}=", v) }
+    end
+
+    def self.image(**kwargs)
+      new_with_validation(file_nature: :image, **kwargs)
+    end
+
+    SCHEMA = Dry::Validation.Schema do
+      required(:file_nature).filled(included_in?: VALID_FILE_NATURES)
+      required(:file_type).filled(included_in?: SUPPORTED_FILE_TYPES)
+      optional(:width_px).filled(:int?)
+      optional(:height_px).filled(:int?)
+      optional(:has_multiple_frames).filled(:bool?)
+    end
+
+    def self.new_with_validation(**attributes)
       result = schema.call(**attributes)
       if result.success?
-        result.to_h
+        new(**result)
       else
         raise ParsingError, "Parsing failed with error: #{result.errors}"
       end
     end
-    # # What kind of file is it?
-    # attribute :file_nature, Types::Strict::Symbol
-    #
-    # # What filetype was recognized? Will contain a non-ambiguous symbol
-    # # referring to the file format. The symbol can be used as a filename
-    # # extension safely
-    # attribute :file_type, Types::Strict::Symbol
-    #
-    # # Number of pixels horizontally in the pixel buffer
-    # attribute :width_px, Types::Strict::Int.default(0)
-    #
-    # # Number of pixels vertically in the pixel buffer
-    # attribute :height_px, Types::Strict::Int.default(0)
-    #
-    # # Whether the file has multiple frames (relevant for image files and video)
-    # attribute :has_multiple_frames, Types::Strict::Bool.default(false)
-    #
-    # # Image orientation value from EXIF. Can be between 1-9.
-    # # Some guidlines for using this number can be found here
-    # # https://beradrian.wordpress.com/2008/11/14/rotate-exif-images/
-    # attribute :exif_orientation, Types::Strict::Int.optional
-
-    def self.image(**attributes)
-      validate_and_return(file_nature: :image, **attributes)
-    end
-
   end
 end
