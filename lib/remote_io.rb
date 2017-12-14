5397ccd75c2b7901bf4f379afab7c64deaf28bc4
Julik Tarkhanov <me@julik.nl>
2017-12-14 16:03:37 +0100

    Fold size request into the first read() call

--- lib/remote_io.rb
+++ lib/remote_io.rb
@@ -2,25 +2,23 @@ class FormatParser::RemoteIO
   # @param uri[URI, String] the remote URL to obtain
   def initialize(uri)
     require 'net/http'
-
     @uri = URI(uri)
     @pos = 0
     @remote_size = false
   end
 
   # Emulates IO#seek
-  def seek(offset, mode = IO::SEEK_SET)
-    raise "Unsupported read mode #{mode}" unless mode == IO::SEEK_SET
-    @remote_size ||= request_object_size
-    @pos = clamp(0, offset, @remote_size)
-    0 # always return 0!
+  def seek(offset)
+    @pos = offset
+    0 # always return 0
   end
 
   # Emulates IO#size.
   #
   # @return [Fixnum] the size of the remote resource
   def size
-    @remote_size ||= request_object_size
+    raise "Remote size not yet obtained, need to perform at least one read() to get it" unless @remote_size
+    @remote_size
   end
 
   # Emulates IO#read, but requires the number of bytes to read
@@ -31,32 +29,11 @@ class FormatParser::RemoteIO
   #
   # @param n_bytes[Fixnum, nil] how many bytes to read, or `nil` to read all the way to the end
   # @return [String] the read bytes
-  def read(n_bytes = nil)
-    @remote_size ||= request_object_size
-
-    # If the resource is empty there is nothing to read
-    return nil if @remote_size.zero?
-
-    maximum_avaialable = @remote_size - @pos
-    n_bytes ||= maximum_avaialable # nil == read to the end of file
-    return '' if n_bytes.zero?
-    raise ArgumentError, "No negative reads(#{n_bytes})" if n_bytes < 0
-
-    n_bytes = clamp(0, n_bytes, maximum_avaialable)
-
-    read_n_bytes_from_remote(@pos, n_bytes).tap do |data|
-      if data.bytesize != n_bytes
-        raise "Remote read returned #{data.bytesize} bytes instead of #{n_bytes} as requested"
-      end
-      @pos = clamp(0, @pos + data.bytesize, @remote_size)
-    end
-  end
-
-  # Returns the current pointer position within the IO
-  #
-  # @return [Fixnum]
-  def tell
-    @pos
+  def read(n_bytes)
+    http_range = (@pos..(@pos + n_bytes - 1))
+    available_size, body = request_range(http_range)
+    @remote_size ||= available_size
+    body
   end
 
   protected
@@ -69,21 +46,14 @@ class FormatParser::RemoteIO
     request = Net::HTTP::Get.new(@uri)
     request.range = range
     http = Net::HTTP.start(@uri.hostname, @uri.port)
-    http.request(request).body
-  end
+    response = http.request(request)
 
-  # Only used internally when reading the remote ZIP.
-  #
-  # @return [Fixnum] the byte size of the ranged request
-  def request_object_size
-    http = Net::HTTP.start(@uri.hostname, @uri.port)
-    http.request_head(@uri)['Content-Length'].to_i
-  end
+    range_header = response['Content-Range']
+    raise "No range support at #{@url}" unless range_header
 
-  # Reads N bytes at offset from remote
-  def read_n_bytes_from_remote(start_at, n_bytes)
-    range = (start_at..(start_at + n_bytes - 1))
-    request_range(range)
+    # "Content-Range"=>"bytes 0-0/307404381"
+    size = range_header[/\/(\d+)$/, 1].to_i
+    [size, response.body]
   end
 
   # Reads the Content-Length and caches it
