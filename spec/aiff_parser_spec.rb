d31351f39db31e6f7d9570c3b5b3e24475dd36cb
Noah Berman <noah@wetransfer.com>
2017-12-20 13:37:03 +0100

    Add apple AIF and test values

--- spec/aiff_parser_spec.rb
+++ spec/aiff_parser_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FormatParser::AIFFParser do
-  it 'parses a sample file' do
+  it 'parses an AIFF sample file' do
     parse_result = subject.information_from_io(File.open(__dir__ + '/fixtures/AIFF/fixture.aiff', 'rb'))
 
     expect(parse_result.file_nature).to eq(:audio)
@@ -9,5 +9,15 @@ describe FormatParser::AIFFParser do
     expect(parse_result.num_audio_channels).to eq(2)
     expect(parse_result.audio_sample_rate_hz).to be_within(0.01).of(44100)
     expect(parse_result.media_duration_seconds).to be_within(0.01).of(1.05)
+  end
+
+  it 'parses a Logic Pro created AIF sample file' do
+    parse_result = subject.information_from_io(File.open(__dir__ + '/fixtures/AIFF/fixture-logic-aiff.aif', 'rb'))
+
+    expect(parse_result.file_nature).to eq(:audio)
+    expect(parse_result.media_duration_frames).to eq(302400)
+    expect(parse_result.num_audio_channels).to eq(2)
+    expect(parse_result.audio_sample_rate_hz).to be_within(0.01).of(44100)
+    expect(parse_result.media_duration_seconds).to be_within(0.01).of(6.85)
   end
 end
