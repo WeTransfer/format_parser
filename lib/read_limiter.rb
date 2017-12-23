754e665f020b0e9bac90dd7b1c51067d7d427612
Julik Tarkhanov <me@julik.nl>
2017-12-23 01:05:35 +0100

    Add a read limiter
    We want to make sure that if a parser gets derailed into a read loop
    because the format is not applicable (or invalid) we are able
    to forcibly stop the parser and move on

---
+++ lib/read_limiter.rb
@@ -0,0 +1,40 @@
+class FormatParser::ReadLimiter
+  NO_LIMIT = nil
+  class BudgetExceeded < StandardError
+  end
+
+  def initialize(io, max_bytes: NO_LIMIT, max_reads: NO_LIMIT, max_seeks: NO_LIMIT)
+    @max_bytes = max_bytes
+    @max_reads = max_reads
+    @max_seeks = max_seeks
+
+    @io = io
+    @seeks = 0
+    @reads = 0
+    @bytes = 0
+  end
+
+  def seek(to_offset)
+    @seeks += 1
+    if @max_seeks && @seeks > @max_seeks
+      raise BudgetExceeded, "Seek budget exceeded (%d seeks performed)" % @max_seeks
+    end
+    @io.seek(to_offset)
+  end
+
+  def read(n)
+    @bytes += n
+    @reads += 1
+
+    if @max_bytes && @bytes > @max_bytes
+      raise BudgetExceeded, "Read bytes budget (%d) exceeded" % @max_bytes
+    end
+
+    if @max_reads && @reads > @max_reads
+      raise BudgetExceeded, "Number of read() calls exceeded (%d max)" % @max_reads
+    end
+
+    @io.read(n)
+  end
+end
+
---
+++ spec/read_limiter_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "ReadLimiter" do
+  let(:io) { StringIO.new(Random.new.bytes(1024)) }
+
+  it 'does not enforce any limits with default arguments' do
+    reader = FormatParser::ReadLimiter.new(io)
+    2048.times { reader.seek(1) }
+    2048.times { reader.read(4) }
+  end
+
+  it 'enforces the number of seeks' do
+    reader = FormatParser::ReadLimiter.new(io, max_seeks: 4)
+    4.times { reader.seek(1) }
+    expect {
+      reader.seek(1)
+    }.to raise_error(/Seek budget exceeded/)
+  end
+
+  it 'enforces the number of reads' do
+    reader = FormatParser::ReadLimiter.new(io, max_reads: 4)
+    4.times { reader.read(1) }
+    expect {
+      reader.read(1)
+    }.to raise_error(/calls exceeded \(4 max\)/)
+  end
+
+  it 'enforces the number of bytes read' do
+    reader = FormatParser::ReadLimiter.new(io, max_bytes: 512)
+    reader.read(512)
+    expect {
+      reader.read(1)
+    }.to raise_error(/bytes budget \(512\) exceeded/)
+  end
+end
