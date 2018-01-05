5293e0c14062b85c89ee01e6643239ca714aebd0
Julik Tarkhanov <me@julik.nl>
2018-01-06 00:00:08 +0100

    Always prefetch the first page upfront
    to hydrate the size value of remote IO handles,
    which requires an HTTP request to populate anyway.
    Some parsers will want to start parsing from the tail,
    and to do this they will desire to call `size` before
    performing the first `read()`

--- lib/format_parser.rb
+++ lib/format_parser.rb
@@ -24,6 +24,12 @@ module FormatParser
   def self.parse(io)
     io = Care::IOWrapper.new(io) unless io.is_a?(Care::IOWrapper)
 
+    # Prefetch the first page, since it is very likely to be touched
+    # by all parsers anyway. Additionally, when using RemoteIO we need
+    # to obtain the size of the resource, which is only available
+    # after having performed the first GET - at least on S3
+    io.read(1); io.seek(0)
+
     # Always instantiate parsers fresh for each input, since they might
     # contain instance variables which otherwise would have to be reset
     # between invocations, and would complicate threading situations
