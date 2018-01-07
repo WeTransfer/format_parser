2e421c8ffdece644e536bbbe34619d5b0df31867
Julik Tarkhanov <me@julik.nl>
2018-01-08 00:48:36 +0100

    Meh

--- lib/parsers/mp3_parser.rb
+++ lib/parsers/mp3_parser.rb
@@ -7,9 +7,15 @@ class FormatParser::MP3Parser
   def information_from_io(io)
     # Read the last 128 bytes which might contain ID3v1
     id3_v1 = ID3V1.attempt_id3_v1_extraction(io)
+    io.seek(0)
     id3_v2 = ID3V2.attempt_id3_v2_extraction(io)
+
     raise id3_v2.inspect
   end
+
+  def parse_mpeg_frame(io)
+  end
+
 
   FormatParser.register_parser_constructor self
 end
