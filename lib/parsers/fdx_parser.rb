2461cef0e178f97dca039b7d3d668c233e14172f
Noah Berman <noah@noahberman.org>
2018-01-13 18:43:57 +0100

    Add checks for non-FDX files and fix gotofail

--- lib/parsers/fdx_parser.rb
+++ lib/parsers/fdx_parser.rb
@@ -2,13 +2,10 @@ class FormatParser::FDXParser
   include FormatParser::IOUtils
 
   def information_from_io(io)
-    if xml_check(io)
-      file_and_document_type = safe_read(io, 100)
-      file_type, document_type = check_for_document_type(file_and_document_type)
-    else
-      return # Bail if it's not even XML
-    end
-
+    return if !xml_check(io)
+    file_and_document_type = safe_read(io, 100)
+    file_type, document_type = check_for_document_type(file_and_document_type)
+    return if file_type != :fdx
     file_info = FormatParser::FileInformation.new(
       file_nature: :document,
       file_type: file_type,
@@ -20,7 +17,7 @@ class FormatParser::FDXParser
     xml_check = safe_read(io, 5)
     xml_check == "<?xml" ? true : false
   end
-
+    
   def check_for_document_type(file_and_document_type)
     sanitized_data = file_and_document_type.downcase
     if sanitized_data.include?("finaldraft") && sanitized_data.include?("script") 
@@ -30,4 +27,5 @@ class FormatParser::FDXParser
     end
   end
 
+  FormatParser.register_parser_constructor self
 end
--- spec/parsers/fdx_parser_spec.rb
+++ spec/parsers/fdx_parser_spec.rb
@@ -2,13 +2,22 @@ require 'spec_helper'
 
 describe FormatParser::FDXParser do
   describe 'is able to parse the sample file' do
-    Dir.glob(fixtures_dir + '/*.fdx').each do |fdx_path|
+    Dir.glob(fixtures_dir + '/XML/*.fdx').each do |fdx_path|
       it "is able to parse #{File.basename(fdx_path)}" do
         parsed = subject.information_from_io(File.open(fdx_path, 'rb'))
         expect(parsed).not_to be_nil
         expect(parsed.file_nature).to eq(:document)
         expect(parsed.file_type).to eq(:fdx)
         expect(parsed.document_type).to eq(:script)
+      end
+    end
+  end
+  
+  describe 'does not parse other XML files as FDX' do
+    Dir.glob(fixtures_dir + '/*.svg').each do |svg_path|
+      it 'returns nil when parsing a non-fdx xml file' do
+        parsed = subject.information_from_io(File.open(svg_path, 'rb'))
+        expect(parsed).to eq(nil)
       end
     end
   end
