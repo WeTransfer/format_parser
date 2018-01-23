aeacdeb6ff0afde429e2ef3310244982b1e08089
Noah Berman <noah@wetransfer.com>
2018-01-23 16:26:36 +0100

    Return error in Care for negative bytes to read

--- lib/care.rb
+++ lib/care.rb
@@ -26,7 +26,8 @@ class Care
     end
 
     def read(n_bytes)
-      return '' if (n_bytes == 0 || n_bytes < 0) # As hardcoded for all Ruby IO objects
+      return '' if n_bytes == 0 # As hardcoded for all Ruby IO objects
+      raise ArgumentError, "negative length #{n_bytes} given" if n_bytes < 0 # also as per Ruby IO objects
       read = @cache.byteslice(@io, @pos, n_bytes)
       return unless read && !read.empty?
       @pos += read.bytesize
