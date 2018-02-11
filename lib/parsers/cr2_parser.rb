d96834341e88d1439bc9b436910183aac22ca09f
onur <onurkucukkece@gmail.com>
2018-02-11 17:27:49 +0300

    added a method for parsing ifd0 entries

--- lib/parsers/cr2_parser.rb
+++ lib/parsers/cr2_parser.rb
@@ -9,4 +9,15 @@ class FormatParser::CR2Parser
     # Check whether it's a CR2 file
     return unless cr2_check_bytes == 'CR'
   end
+  def parse_ifd(io, offset)
+    io.seek(offset)
+    entries_count = safe_read(io, 2).reverse.bytes.collect{ |c| c.to_s(16) }.join.hex
+    entries_count.times do |index|
+      entry = safe_read(io, 12)
+      id = entry[0..1].bytes.reverse.map { |b| sprintf("%02X",b) }.join.hex
+      type = entry[2..3].bytes.reverse.map { |b| sprintf("%02X",b) }.join.hex
+      count = entry[4..7].bytes.reverse.map { |b| sprintf("%02X",b) }.join.hex
+      value = entry[8..11].bytes.reverse.map { |b| sprintf("%02X",b) }.join
+    end
+  end
 end
