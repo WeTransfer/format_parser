22fdd06f90952a6a2dac3e2f05cb0541168832bb
westerg <grdwandr@gmail.com>
2018-03-04 09:53:41 +0100

    Rewritten the PDF parser
    * Reads byte by byte to scan for PDF headers
    * Returns an InvalidStructure when no headers are found or an EOF

--- lib/parsers/pdf_parser.rb
+++ lib/parsers/pdf_parser.rb
@@ -14,32 +14,62 @@ class FormatParser::PDFParser
   # this. The only way of solving this correctly is by adding
   # different types of PDF's in the specs.
   #
-  COUNT_MARKER = /\/(N|Count|Page)\s([0-9]+)/
+  COUNT_MARKERS = ['Count ']
+  EOF_MARKER    = '%EOF'
 
   def call(io)
     io = FormatParser::IOConstraint.new(io)
 
     return unless safe_read(io, 9) =~ PDF_MARKER
 
+    attributes = scan_for_attributes(io)
+
     FormatParser::Document.new(
       format: :pdf,
-      page_count: page_count(io)
+      page_count: attributes[:page_count]
     )
   end
 
   private
 
-  def page_count(io)
-    io.seek(9)
+  # Read ahead bytes until one of % or / is reached.
+  # A header in a PDF always starts with a /
+  # The % is to detect the EOF
+  #
+  def scan_for_attributes(io)
+    result = {}
 
-    while read = safe_read(io, 100)
-      if count = read.match(COUNT_MARKER)
-        page_count = count.captures.last.to_i
-        break
+    while read = safe_read(io, 1)
+      case read
+      when '%'
+        break if safe_read(io, EOF_MARKER.size) == EOF_MARKER
+      when '/'
+        find_page_count(io, result)
       end
     end
 
-    page_count
+    result
+  end
+
+  def find_page_count(io, result)
+    COUNT_MARKERS.each do |marker|
+      if safe_read(io, marker.size) == marker
+        result[:page_count] = read_numbers(io)
+      end
+    end
+  end
+
+  # Read ahead bytes until no more numbers are found
+  # This assumes that the position of io starts at a
+  # number
+  def read_numbers(io)
+    numbers = ''
+
+    while c = safe_read(io, 1)
+      c =~ /\d+/ ? numbers << c : break
+    end
+
+    numbers.to_i
   end
 
   FormatParser.register_parser self, natures: :document, formats: :pdf
--- spec/parsers/pdf_parser_spec.rb
+++ spec/parsers/pdf_parser_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe FormatParser::PDFParser do
-  shared_examples :behave_like_pdf do |hash|
-    let(:parsed_pdf) {
-      subject.call(
-        File.open(fixtures_dir + '/' + hash.fetch(:file), 'rb')
+  let(:parsed_pdf) {
+    subject.call(
+      File.open(
+        Pathname.new(fixtures_dir).join('PDF').join(pdf_file),
+        'rb'
       )
-    }
+    )
+  }
+
+  shared_examples :behave_like_pdf do |hash|
+    let(:pdf_file) { hash.fetch(:file) }
 
     it 'acts as a pdf' do
       expect(parsed_pdf).not_to be_nil
@@ -19,6 +24,34 @@ describe FormatParser::PDFParser do
     end
   end
 
+  describe 'a PDF file with a missing version header' do
+    let(:pdf_file) { 'not_a.pdf' }
+
+    it 'does not parse succesfully' do
+      expect(parsed_pdf).to be_nil
+    end
+  end
+
+  describe 'a PDF file with a correct header but no valid content' do
+    let(:pdf_file) { 'broken.pdf' }
+
+    pending 'does not parse succesfully'
+  end
+
+  describe 'exceeding the PDF read limit' do
+    let(:pdf_file) { 'read_limit.pdf' }
+
+    pending 'does not parse succesfully'
+  end
+
+  describe 'a PDF file with a missing COUNT_HEADER' do
+    let(:pdf_file) { 'missing_page_count.pdf' }
+
+    it 'does not return a page count' do
+      expect(parsed_pdf.page_count).to eq(nil)
+    end
+  end
+
   describe 'parses a PDF file' do
     describe 'a single page file' do
       include_examples :behave_like_pdf, file: '1_page.pdf', page_count: 1
@@ -26,6 +59,10 @@ describe FormatParser::PDFParser do
 
     describe 'a multi page pdf file' do
       include_examples :behave_like_pdf, file: '2_pages.pdf', page_count: 2
+    end
+
+    describe 'a multi page pdf file with content' do
+      include_examples :behave_like_pdf, file: '10_pages.pdf', page_count: 10
     end
   end
 end
