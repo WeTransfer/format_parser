172dd4e90d6477b850c9d2a4022c6ea262c07437
Julik Tarkhanov <me@julik.nl>
2018-04-21 01:11:10 +0200

    Make sure MP3 parser skips infinite duration
    When a file is mislabeled as MP3 the resulting duration
    calculation will be incorrect. If that happens, a token value
    we can detect before returning the result is the duration
    of Float::INFINITY. Upon detecting that value we will
    exit the parser without returning the result.
    
    Should close #79

--- spec/parsers/mp3_parser_spec.rb
+++ spec/parsers/mp3_parser_spec.rb
@@ -44,4 +44,11 @@ describe FormatParser::MP3Parser do
 
     expect(parsed.intrinsics).not_to be_nil
   end
+
+  it 'avoids returning a result when the parsed duration is infinite' do
+    fpath = fixtures_dir + '/JPEG/UNVIEWABLE_many_APP1_markers_before_sos.jpg'
+    parsed = subject.call(File.open(fpath, 'rb'))
+
+    expect(parsed).to be_nil
+  end
 end
