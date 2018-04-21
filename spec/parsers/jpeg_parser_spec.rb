WeTransfer/format_parser

commit ac9fbbbd6fed174027c750a0bf5be3e0ee49b05c

Speed up JPEG parser for invalid inputs (#105)
Previously we could keep scanning JPEG files until reaching the read limits,
which was also slow because of the incurred read/unpack/first calls for each
byte. This could lead to parse calls for, say, PSD files take 3-4 seconds since
we would be looking for a marker and not finding it and would just keep reading
one byte at a time.

This will make read_next_marker fail earlier if it cannot find a marker sequence
after "blind scanning" 1024 bytes.

lib/parsers/jpeg_parser.rb
@@ -10,6 +10,7 @@ class FormatParser::JPEGParser
   SOS_MARKER  = 0xDA  # start of stream
   APP1_MARKER = 0xE1  # maybe EXIF
   EXIF_MAGIC_STRING = "Exif\0\0".b
+  MAX_GETBYTE_CALLS_WHEN_LOOKING_FOR_MARKER = 1023
 
   def call(io)
     @buf = FormatParser::IOConstraint.new(io)
@@ -78,11 +79,17 @@ class FormatParser::JPEGParser
   end
 
   # Read a byte, if it is 0xFF then skip bytes as long as they are also 0xFF (byte stuffing)
-  # and return the first byte scanned that is not 0xFF
+  # and return the first byte scanned that is not 0xFF. Also applies limits so that we do not
+  # read for inordinate amount of time should we encounter a file where we _do_ have a SOI
+  # marker at the start and then no markers for a _very_ long time (happened with some PSDs)
   def read_next_marker
-    c = read_char while c != 0xFF
-    c = read_char while c == 0xFF
-    c
+    max_two_byte_reads = MAX_GETBYTE_CALLS_WHEN_LOOKING_FOR_MARKER / 2
+    max_two_byte_reads.times do
+      previous = read_char
+      current = read_char
+      return current if previous == 0xFF && current != 0xFF
+    end
+    nil # Nothing found
   end
 
   def scan_start_of_frame

spec/parsers/jpeg_parser_spec.rb
@@ -49,6 +49,23 @@ describe FormatParser::JPEGParser do
     expect(result.intrinsics).to eq(exif_pixel_x_dimension: 8214, exif_pixel_y_dimension: 5476)
   end
 
+  it 'does not continue parsing for inordinate amount of time if the file contains no 0xFF bytes' do
+    # Create a large fuzzed input that consists of any bytes except 0xFF,
+    # so that the marker detector has nothing to latch on to
+    bytes_except_byte_255 = 0x0..0xFE
+
+    # Start the blob with the usual SOI marker - 0xFF 0xD8, so that the parser does not
+    # bail out too early and actually "bites" into the blob
+    no_markers = ([0xFF, 0xD8] + (16 * 1024).times.map { rand(bytes_except_byte_255) }).pack('C*')
+
+    # Yes, assertions on a private method - but we want to ensure we do not read more
+    # single bytes than the restriction stipulates we may. At the same time we check that
+    # the method does indeed, get triggered
+    expect(subject).to receive(:read_char).at_least(100).times.at_most(1024).times.and_call_original
+    result = subject.call(StringIO.new(no_markers))
+    expect(result).to be_nil
+  end
+
   it 'does not return a result for a Keynote document' do
     key_path = fixtures_dir + '/JPEG/keynote_recognized_as_jpeg.key'
     result = subject.call(File.open(key_path, 'rb'))

commit d09b96f18d2f4065fbfe6fec2ae67ba346062912

Fix JPEG parsing regression
In ac9fbbbd6fed174027c750a0bf5be3e0ee49b05c we introduced
an optimization when looking for the next marker. It was
implemented using "reads in blocks of 2 bytes", which is
actually a bug since we are looking for a _sequence_
of bytes, and that sequence may end up smashed between
two of these blocks. Replace this way of reading with
reads "in groups of 2 byes" where you can examine the
tuple of the two bytes individually.

lib/parsers/jpeg_parser.rb
@@ -10,7 +10,7 @@ class FormatParser::JPEGParser
   SOS_MARKER  = 0xDA  # start of stream
   APP1_MARKER = 0xE1  # maybe EXIF
   EXIF_MAGIC_STRING = "Exif\0\0".b
-  MAX_GETBYTE_CALLS_WHEN_LOOKING_FOR_MARKER = 1023
+  MUST_FIND_NEXT_MARKER_WITHIN_BYTES = 1024
 
   def call(io)
     @buf = FormatParser::IOConstraint.new(io)
@@ -85,11 +85,12 @@ class FormatParser::JPEGParser
   # read for inordinate amount of time should we encounter a file where we _do_ have a SOI
   # marker at the start and then no markers for a _very_ long time (happened with some PSDs)
   def read_next_marker
-    max_two_byte_reads = MAX_GETBYTE_CALLS_WHEN_LOOKING_FOR_MARKER / 2
-    max_two_byte_reads.times do
-      previous = read_char
-      current = read_char
-      return current if previous == 0xFF && current != 0xFF
+    # We need to find a sequence of two bytes - the first one is 0xFF, the other is anything but 0xFF
+    a = read_char
+    (MUST_FIND_NEXT_MARKER_WITHIN_BYTES - 1).times do
+      b = read_char
+      return b if a == 0xFF && b != 0xFF # Caught the marker
+      a = b # Shift the tuple one byte forward
     end
     nil # Nothing found
   end

spec/parsers/jpeg_parser_spec.rb
@@ -75,14 +75,21 @@ describe FormatParser::JPEGParser do
     # Yes, assertions on a private method - but we want to ensure we do not read more
     # single bytes than the restriction stipulates we may. At the same time we check that
     # the method does indeed, get triggered
-    expect(subject).to receive(:read_char).at_least(100).times.at_most(1024).times.and_call_original
+    allow(subject).to receive(:read_char).and_call_original
     result = subject.call(StringIO.new(no_markers))
     expect(result).to be_nil
+    expect(subject).to have_received(:read_char).at_most(1026).times
   end
 
   it 'does not return a result for a Keynote document' do
     key_path = fixtures_dir + '/JPEG/keynote_recognized_as_jpeg.key'
     result = subject.call(File.open(key_path, 'rb'))
     expect(result).to be_nil
+  end
+
+  it 'parses the the marker structure correctly when marker bytes cannot be read in groups of 2' do
+    kitten_path = fixtures_dir + '/JPEG/off-cadence-markers.jpg'
+    result = subject.call(File.open(kitten_path, 'rb'))
+    expect(result).not_to be_nil
   end
 end