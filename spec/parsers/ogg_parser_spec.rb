6db8d70ae966a9e50f455e4c3e84e5e8702c136e
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-05-16 22:30:26 +0200

    Add checksum verification

--- spec/parsers/ogg_parser_spec.rb
+++ spec/parsers/ogg_parser_spec.rb
@@ -15,4 +15,14 @@ describe FormatParser::OggParser do
     parse_result = subject.call(File.open(__dir__ + '/../fixtures/Ogg/with_garbage_at_the_end.ogg', 'rb'))
     expect(parse_result).to be_nil
   end
+
+  it "correctly parses an ogg file when a magic string occurs in the page's body" do
+    parse_result = subject.call(File.open(__dir__ + '/../fixtures/Ogg/with_confusing_magic_string.ogg', 'rb'))
+
+    expect(parse_result.nature).to eq(:audio)
+    expect(parse_result.format).to eq(:ogg)
+    expect(parse_result.num_audio_channels).to eq(1)
+    expect(parse_result.audio_sample_rate_hz).to eq(8000)
+    expect(parse_result.media_duration_seconds).to be_within(0.01).of(0.45)
+  end
 end
