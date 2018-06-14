f2724abdd7696325f1c4418a9cb6b984a4a03b76
Julik Tarkhanov <me@julik.nl>
2018-06-14 13:48:11 +0200

    Deal with reals better

---
+++ spec/parsers/pdf_parser/nu_object_parser.rb
@@ -0,0 +1,152 @@
+class NuObjectParser
+  Malformed = Class.new(RuntimeError)
+  RE = ->(str) { /#{Regexp.escape(str)}/ }
+  STRATEGIES = {
+    RE["/"]  => :parse_pdf_name,
+    RE["<<"] => :parse_dictionary,
+    RE["["]  => :parse_array,
+    RE["("]  => :parse_string,
+    RE["<"]  => :parse_hex_string,
+    /\d+ \d+ R/ => :parse_ref,
+
+    RE["true"]  => :wrap,
+    RE["false"] => :wrap,
+    RE["null"]  => :wrap,
+
+    # 34.5 −3.62 +123.6 4. −.002 0.0 are all valid reals
+    /(\-|\+?)(\d+)\.(\d+)/ => :wrap_real,
+    /(\-|\+?)(\d+)\./ => :wrap_real,
+    /(\-|\+?)\.(\d+)/ => :wrap_real,
+    /\-?(\d+)/ => :wrap_int,
+
+    RE["obj"]       => :wrap,
+    RE["endobj"]    => :wrap,
+    RE["stream"]    => :wrap,
+    RE["endstream"] => :wrap,
+
+    /\s+/           => :wrap_whitespace,
+    /./             => :garbage,
+  }
+
+  STRING_ESCAPES = {
+    "\r"   => "\n",
+    "\n\r" => "\n",
+    "\r\n" => "\n",
+    "\\n"  => "\n",
+    "\\r"  => "\r",
+    "\\t"  => "\t",
+    "\\b"  => "\b",
+    "\\f"  => "\f",
+    "\\("  => "(",
+    "\\)"  => ")",
+    "\\\\" => "\\",
+    "\\\n" => "",
+  }
+  0.upto(9)   { |n| STRING_ESCAPES["\\00" + n.to_s] = ("00"+n.to_s).oct.chr }
+  0.upto(99)  { |n| STRING_ESCAPES["\\0" + n.to_s]  = ("0"+n.to_s).oct.chr }
+  0.upto(377) { |n| STRING_ESCAPES["\\" + n.to_s]   = n.to_s.oct.chr }
+
+  def wrap_real(pattern)
+    [:real, @sc.scan(pattern).to_f]
+  end
+
+  def wrap_int(pattern)
+    [:int, @sc.scan(pattern).to_i]
+  end
+
+  def wrap_whitespace(pattern)
+    @sc.scan(pattern)
+    [:whitespace, nil]
+  end
+
+  def wrap(pattern)
+    [:lit, @sc.scan(pattern).to_sym]
+  end
+
+  def consume!(pattern, method_name)
+    at = @sc.pos
+    unless @sc.check(pattern)
+      $stderr.puts " : #{pattern} -> #{method_name} @#{at}: will scan #{@sc.peek(8).inspect}..."
+      return false
+    end
+    $stderr.puts   "M: #{pattern} -> #{method_name} @#{at}: will scan #{@sc.peek(8).inspect}..."
+    result = send(method_name, pattern)
+    @token_stream << result unless result == [:whitespace, nil]
+    true
+  end
+
+  def parse_ref(start_pattern)
+    [:ref, @sc.scan(start_pattern)]
+  end
+
+  def parse_array(start_pattern)
+    @sc.scan(start_pattern) # consume [
+    dict_open_at = @token_stream.length
+    walk_scanner(RE["]"])
+    raise Malformed, "Array did not terminate" unless @token_stream.pop == :terminator
+    array_items = @token_stream.pop(@token_stream.length - dict_open_at)
+    [:array, array_items]
+  end
+
+  def parse_dictionary(start_pattern)
+    @sc.scan(start_pattern) # consume <<
+    dict_open_at = @token_stream.length
+    walk_scanner(RE[">>"])
+    raise Malformed, "Dictionary did not terminate" unless @token_stream.pop == :terminator
+    dict_items = @token_stream.pop(@token_stream.length - dict_open_at)
+    [:dict, dict_items]
+  end
+
+  def parse_hex_string(start_pattern)
+    str = @sc.scan(/<[0-9a-f]+>/i)
+    raise Malformed, "Malformed hex string at #{@sc.pos}" unless str
+
+    str << "0" unless str.bytesize % 2 == 0
+    hex_str = str.scan(/../).map {|i| i.hex.chr}.join
+    [:hex_string, hex_str]
+  end
+
+  def parse_string(start_pattern)
+    rest_of_string = @sc.scan_until(/[^\\]\)/) # consume everything starting with ( and upto a non-escaped )
+    raise Malformed, "String did not terminate (started at at #{@sc.pos})" unless rest_of_string
+    rest_of_string[1..-2].gsub (/\\([nrtbf()\\\n]|\d{1,3})?|\r\n?|\n\r/m) do |match|
+      STRING_ESCAPES[match] || ""
+    end
+  end
+
+  def parse_pdf_name(start_pattern)
+    letters = ('a'..'z').to_a.join + ('A'..'Z').to_a.join
+    name = @sc.scan(/\/[#{letters}\d]+/)
+    raise Malformed, "Expected a well-formed PDF name at #{@sc.pos} but could not recover any" unless name
+    [:name, name]
+  end
+
+  def garbage(*)
+    raise Malformed, "Expected a meaningful token at #{@sc.pos} but did not encounter one"
+  end
+
+  def walk_scanner(halt_at_pattern)
+    (@sc.string.bytesize - @sc.pos).times do
+      # Terminate if EOS reached
+      break if @sc.eos?
+
+      # Terminate early
+      if halt_at_pattern && halted = @sc.scan(halt_at_pattern)
+        @token_stream << :terminator
+        return
+      end
+
+      # Walk through STRATEGIES and stop iterating on first non-false call to consume!
+      STRATEGIES.find do |pattern, method_name|
+        consume!(pattern, method_name)
+      end
+    end
+  end
+
+  def parse(str)
+    @sc = StringScanner.new(str)
+    @token_stream = []
+    walk_scanner(_stop_at_pattern = nil)
+    @token_stream
+  end
+end
--- spec/parsers/pdf_parser/object_parser_spec.rb
+++ spec/parsers/pdf_parser/object_parser_spec.rb
@@ -1,177 +1,15 @@
 require 'spec_helper'
-
-class NuObjectParser
-  Malformed = Class.new(RuntimeError)
-  RE = ->(str) { /#{Regexp.escape(str)}/ }
-  STRATEGIES = {
-    RE["/"]  => :parse_pdf_name,
-    RE["<<"] => :parse_dictionary,
-    RE["["]  => :parse_array,
-    RE["("]  => :parse_string,
-    RE["<"]  => :parse_hex_string,
-    /\d+ \d+ R/ => :parse_ref,
-
-    RE["true"]  => :wrap,
-    RE["false"] => :wrap,
-    RE["null"]  => :wrap,
-
-    /\-?(\d+)\.(\d+)/ => :wrap_real,
-    /\-?(\d+)/ => :wrap_int,
-
-    RE["obj"]       => :wrap,
-    RE["endobj"]    => :wrap,
-    RE["stream"]    => :wrap,
-    RE["endstream"] => :wrap,
-#    RE[">>"]        => :wrap,
-#    RE["]"]         => :wrap,
-#    RE[">"]         => :wrap,
-#    RE[")"]         => :wrap,
-
-    /\s+/           => :wrap_whitespace,
-    /./             => :garbage,
-  }
-
-  STRING_ESCAPES = {
-    "\r"   => "\n",
-    "\n\r" => "\n",
-    "\r\n" => "\n",
-    "\\n"  => "\n",
-    "\\r"  => "\r",
-    "\\t"  => "\t",
-    "\\b"  => "\b",
-    "\\f"  => "\f",
-    "\\("  => "(",
-    "\\)"  => ")",
-    "\\\\" => "\\",
-    "\\\n" => "",
-  }
-  0.upto(9)   { |n| STRING_ESCAPES["\\00" + n.to_s] = ("00"+n.to_s).oct.chr }
-  0.upto(99)  { |n| STRING_ESCAPES["\\0" + n.to_s]  = ("0"+n.to_s).oct.chr }
-  0.upto(377) { |n| STRING_ESCAPES["\\" + n.to_s]   = n.to_s.oct.chr }
-
-  def wrap_true(sc, pattern)
-    @sc.scan(pattern)
-    true
-  end
-
-  def wrap_false(pattern)
-    @sc.scan(pattern)
-    false
-  end
-  
-  def wrap_nil(pattern)
-    @sc.scan(pattern)
-    nil
-  end
-
-  def wrap_real(pattern)
-    @sc.scan(pattern).to_f
-  end
-
-  def wrap_int(pattern)
-    @sc.scan(pattern).to_i
-  end
-
-  def wrap_whitespace(pattern)
-    @sc.scan(pattern)
-    :whitespace
-  end
-
-  def wrap(pattern)
-    data = @sc.scan(pattern)
-    data.to_sym
-  end
-
-  def consume!(pattern, method_name)
-    return unless @sc.check(pattern)
-    at = @sc.pos
-    result = send(method_name, pattern)
-    @token_stream << result unless result == :whitespace
-    true
-  end
-
-  def parse_ref(start_pattern)
-    [:ref, @sc.scan(start_pattern)]
-  end
-
-  def parse_array(start_pattern)
-    @sc.scan(start_pattern) # consume [
-    dict_open_at = @token_stream.length
-    walk_scanner(RE["]"])
-    raise Malformed, "Dictionary did not terminate" unless @token_stream.pop == :terminator
-    array_items = @token_stream.pop(@token_stream.length - dict_open_at)
-    [:array, array_items]
-  end
-
-  def parse_dictionary(start_pattern)
-    @sc.scan(start_pattern) # consume <<
-    dict_open_at = @token_stream.length
-    walk_scanner(RE[">>"])
-    raise Malformed, "Dictionary did not terminate" unless @token_stream.pop == :terminator
-    dict_items = @token_stream.pop(@token_stream.length - dict_open_at)
-    [:dict, dict_items]
-  end
-
-  def parse_hex_string(start_pattern)
-    str = @sc.scan(/<[0-9a-f]+>/i)
-    raise Malformed, "Malformed hex string at #{@sc.pos}" unless str
-
-    str << "0" unless str.size % 2 == 0
-    hex_str = str.scan(/../).map {|i| i.hex.chr}.join
-    [:hex_string, hex_str]
-  end
-
-  def parse_string(start_pattern)
-    rest_of_string = @sc.scan_until(/[^\\]\)/) # consume everything starting with ( and upto a non-escaped )
-    raise Malformed, "String did not terminate (started at at #{@sc.pos})" unless rest_of_string
-    rest_of_string[1..-2].gsub (/\\([nrtbf()\\\n]|\d{1,3})?|\r\n?|\n\r/m) do |match|
-      STRING_ESCAPES[match] || ""
-    end
-  end
-
-  def parse_pdf_name(start_pattern)
-    letters = ('a'..'z').to_a.join + ('A'..'Z').to_a.join + "/"
-    name = @sc.scan(/\/[#{letters}\d]+/)
-    raise Malformed, "Expected a well-formed PDF name at #{@sc.pos} but could not recover any" unless name
-    [:name, name]
-  end
-
-  def garbage(*)
-    raise Malformed, "Expected a meaningful token at #{@sc.pos} but did not encounter one"
-  end
-
-  def walk_scanner(halt_at_pattern)
-    (@sc.string.bytesize - @sc.pos).times do
-      # Terminate if EOS reached
-      break if @sc.eos?
-
-      # Terminate early
-      if halt_at_pattern && halted = @sc.scan(halt_at_pattern)
-        @token_stream << :terminator
-        return
-      end
-
-      # Walk through STRATEGIES and stop iterating on first non-false call to consume!
-      STRATEGIES.find do |pattern, method_name|
-        consume!(pattern, method_name)
-      end
-    end
-  end
-
-  def parse(str)
-    @sc = StringScanner.new(str)
-    @token_stream = []
-    walk_scanner(_stop_at_pattern = nil)
-    @token_stream
-  end
-end
+require_relative 'nu_object_parser'
 
 describe 'Object parser' do
-  let(:fixture_paths) { Dir.glob(__dir__ + '/*.pdfobj').sort }
-
-  it 'scans the extracted object definitions from the corpus' do
+  describe 'with extracted objects from corpus' do
+    fixture_paths = Dir.glob(__dir__ + '/*.pdfobj').sort
     fixture_paths.each do |path|
-      result = NuObjectParser.new.parse(File.read(path))
+      it "scans #{File.basename(path)}" do
+        result = NuObjectParser.new.parse(File.read(path))
+        require 'pp'
+        pp result
+      end
     end
   end
 
@@ -184,11 +22,11 @@ describe 'Object parser' do
         [:array, [
           [:dict, [
             [:name, "/Name"], "Jim",
-            [:name, "/Age"], 39,
+            [:name, "/Age"], [:int, 39],
             [:name, "/Children"], [:array, ["Heather", "Timothy", "Rebecca"]]]
           ],
-          22,
-          44.55]
+          [:int, 22],
+          [:real, 44.55]]
         ]
       ]
     )
@@ -197,7 +35,7 @@ describe 'Object parser' do
   it 'scans a simple dictionary with strings and ints as values' do
     result = NuObjectParser.new.parse('<</Name (Jim) /Age 25>>')
     expect(result).to eq(
-      [[:dict, [[:name, "/Name"], "Jim", [:name, "/Age"], 25]]]
+      [[:dict, [[:name, "/Name"], "Jim", [:name, "/Age"], [:int, 25]]]]
     )
   end
 
@@ -208,33 +46,52 @@ describe 'Object parser' do
       /Age
         25>>')
     expect(result).to eq(
-      [[:dict, [[:name, "/Name"], "Jim", [:name, "/Age"], 25]]]
+      [[:dict, [[:name, "/Name"], "Jim", [:name, "/Age"], [:int, 25]]]]
+    )
+  end
+
+  it 'parses all kinds of reals' do
+    result = NuObjectParser.new.parse('34.5 -3.62 +123.6 4. -.002 0.0')
+    expect(result).to eq(
+      [[:real, 34.5], [:real, -3.62], [:real, 123.6], [:real, 4.0], [:real, -0.002], [:real, 0.0]]
     )
   end
 
   it 'parses an array of integers' do
     result = NuObjectParser.new.parse('[1 2 3 4]')
     expect(result).to eq(
-      [[:array, [1, 2, 3, 4]]]
+      [[:array, [[:int, 1], [:int, 2], [:int, 3], [:int, 4]]]]
     )
   end
 
   it 'scans an array of integers with one object ref in the middle' do
     result = NuObjectParser.new.parse('[1 20 00 R 3]')
     expect(result).to eq(
-      [[:array, [1, [:ref, "20 00 R"], 3]]]
+      [[:array, [[:int, 1], [:ref, "20 00 R"], [:int, 3]]]]
     )
   end
 
   it 'scans an array of names' do
     result = NuObjectParser.new.parse('[ /Type /Color /Medium/Rare ]')
     expect(result).to eq(
-      [[:array, [[:name, "/Type"], [:name, "/Color"], [:name, "/Medium/Rare"]]]]
+      [[:array, [[:name, "/Type"], [:name, "/Color"], [:name, "/Medium"], [:name, "/Rare"]]]]
+    )
+
+    result = NuObjectParser.new.parse('[/Type/Color/Medium/Rare]')
+    expect(result).to eq(
+      [[:array, [[:name, "/Type"], [:name, "/Color"], [:name, "/Medium"], [:name, "/Rare"]]]]
     )
   end
 
   it 'handles string escapes' do
     result = NuObjectParser.new.parse("(Foo \\(with some bars\\))")
+    expect(result).to eq(
+      ["Foo (with some bars)"]
+    )
+  end
+
+  it 'handles paired braces in strings escapes' do
+    result = NuObjectParser.new.parse("(Foo () bar and (baz))")
     expect(result).to eq(
       ["Foo (with some bars)"]
     )
