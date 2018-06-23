5aab3d412638ff2176c89930eea34f6f7789258e
Ahmed Shahin <ahmed.samir.mohamed88@gmail.com>
2018-06-24 01:27:28 +0200

    Add more specs to ActiveStorage::BlobAnalyzer and ActiveStorage::BlobIO

--- spec/active_storage/blob_analyzer_spec.rb
+++ spec/active_storage/blob_analyzer_spec.rb
@@ -12,30 +12,43 @@ describe FormatParser::ActiveStorage::BlobAnalyzer do
   end
 
   describe '#metadata' do
-    it 'should call FormatParser#parse' do
-      expect(FormatParser).to receive(:parse).with(instance_of(FormatParser::ActiveStorage::BlobIO))
+    context 'internal method calls' do
+      it 'should call FormatParser#parse' do
+        expect(FormatParser).to receive(:parse).with(instance_of(FormatParser::ActiveStorage::BlobIO))
 
-      analyzer.metadata
+        analyzer.metadata
+      end
+
+      it 'should call FormatParser::ReadLimiter.read multiple times' do
+        expect_any_instance_of(FormatParser::ReadLimiter).to receive(:read).at_least(:once)
+
+        analyzer.metadata
+      end
+
+      it 'shoul;d call Care::IOWrapper.read multiple times' do
+        expect_any_instance_of(Care::IOWrapper).to receive(:read).at_least(:once)
+
+        analyzer.metadata
+      end
     end
 
     context 'when parsing an IO no parser can make sense of' do
+      subject { analyzer.metadata }
+
       before do
         allow(blob_service).to receive(:download_chunk) { Random.new.bytes(1) }
       end
-
-      subject { analyzer.metadata }
 
       it { is_expected.to eq({}) }
     end
 
     context 'when parsing an IO recognised by parsers' do
       let(:fixture_path) { fixtures_dir + '/test.png' }
+      subject { analyzer.metadata }
 
       before do
         allow(blob_service).to receive(:download_chunk) { File.open(fixture_path, 'rb').read }
       end
-
-      subject { analyzer.metadata }
 
       it { is_expected.not_to eq({}) }
       it { is_expected.to have_key('nature') }
--- spec/active_storage/blob_io_spec.rb
+++ spec/active_storage/blob_io_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe FormatParser::ActiveStorage::BlobIO do
+  let(:blob_service) { double }
+  let(:blob) { double(key: 'blob_key', service: blob_service) }
+  let(:io) { described_class.new(blob) }
+  let(:fixture_path) { fixtures_dir + '/test.png' }
+
   it_behaves_like 'an IO object compatible with IOConstraint'
+
+  it 'maintains and exposes #pos' do
+    expect(io.pos).to eq(0)
+
+    allow(blob_service).to receive(:download_chunk) { 'a' }
+
+    io.read(1)
+
+    expect(io.pos).to eq(1)
+  end
+
+  describe '#read' do
+    it 'raise error when trying to fetch non existent key' do
+      allow(blob).to receive(:key) { 'invalid_key' }
+      allow(blob_service).to receive(:download_chunk).with(blob.key, (0..0)).and_raise(Errno::ENOENT)
+
+      expect { io.read(1) }.to raise_error(ArgumentError, "Key #{blob.key} does not exist")
+    end
+
+    it 'return the whole file when reading past the end of file' do
+      allow(blob_service).to receive(:download_chunk).with(blob.key, (0..9_999)).and_return('a' * 10)
+
+      io.read(100_00)
+
+      expect(io.size).to eq(10)
+    end
+
+    context 'invalid range' do
+      it 'raise error with -ve range start' do
+      end
+
+      it 'raise when range start greater than range end' do
+        n_bytes = -1
+        range_start = 0 # @pos
+        range_end = range_start + n_bytes - 1
+
+        expect { io.read(n_bytes) }.to raise_error(ArgumentError, "Invalid range from #{range_start} to #{range_end}")
+      end
+    end
+  end
 end
