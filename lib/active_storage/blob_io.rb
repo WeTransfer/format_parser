80f905688427507ecdd066bd08169f51f00a5eed
Ahmed Shahin <ahmed.samir.mohamed88@gmail.com>
2018-06-24 01:26:43 +0200

    Refactor FormatParser::ActiveStorage::BlobIO: - Calculate size - Exception handling:   - range_start > range_end   - negative range_start   - Key does not exist

--- lib/active_storage/blob_io.rb
+++ lib/active_storage/blob_io.rb
@@ -5,6 +5,7 @@ module FormatParser
         @blob = blob
         @service = blob.service
         @pos = 0
+        @remote_size = false
       end
 
       # Emulates IO#seek
@@ -20,15 +21,33 @@ module FormatParser
 
       # Emulates IO#size.
       def size
-        0
+        raise 'Remote size not yet obtained, need to perform at least one read() to retrieve it' unless @remote_size
+        @remote_size
       end
 
       def read(n_bytes)
-        http_range = (@pos..(@pos + n_bytes - 1))
+        range_start = @pos
+        range_end =  @pos + n_bytes - 1
+        http_range = (range_start..range_end)
 
-        body = @service.download_chunk(@blob.key, http_range)
+        # ActiveStorageAdapter#download_chunk method return '' when using range (start > end)
+        # Raise invalid range exception if this is the case to recognise the real empty response
+        raise ArgumentError, "Invalid range from #{range_start} to #{range_end}" if range_start > range_end
 
-        body.force_encoding(Encoding::ASCII_8BIT)
+        begin
+          body = @service.download_chunk(@blob.key, http_range)
+        rescue Errno::ENOENT
+          raise ArgumentError, "Key #{@blob.key} does not exist"
+        rescue Errno::EINVAL
+          # using negative range for example
+          raise ArgumentError, "Invalid range from #{range_start} to #{range_end}"
+        end
+
+        if body
+          @remote_size = body.bytesize
+          @pos += body.bytesize
+          body.force_encoding(Encoding::ASCII_8BIT)
+        end
       end
     end
   end
