245e1fd8085b6944745464dfdd4d576ee40e2708
Julik Tarkhanov <me@julik.nl>
2019-09-16 18:16:45 +0200

    Add support for BMP core headers (#141)
    Adds support for BMP core headers. The example files again are borrowed from fastimage.

--- lib/parsers/bmp_parser.rb
+++ lib/parsers/bmp_parser.rb
@@ -4,7 +4,7 @@ class FormatParser::BMPParser
   include FormatParser::IOUtils
 
   VALID_BMP = 'BM'
-  PERMISSIBLE_PIXEL_ARRAY_LOCATIONS = 40..512
+  PERMISSIBLE_PIXEL_ARRAY_LOCATIONS = 26..512
 
   def likely_match?(filename)
     filename =~ /\.bmp$/i
@@ -21,14 +21,41 @@ class FormatParser::BMPParser
     return unless PERMISSIBLE_PIXEL_ARRAY_LOCATIONS.cover?(pix_array_location)
 
     dib_header = safe_read(io, 40)
+    header_size = dib_header.unpack('V')[0]
+    case header_size
+    when 12 # OS21XBITMAPHEADER
+      parse_bitmap_core_header(dib_header)
+    else # More modern implementations
+      parse_modern_header(dib_header)
+    end
+  end
 
-    _header_size, width, height, _planes, _bits_per_pixel,
+  def parse_bitmap_core_header(dib_header)
+    _header_size, width, height, _num_color_planes, bit_depth = dib_header.unpack('VSSSS')
+
+    # In core bitmap format an unsigned int is used for dimensions,
+    # no inverse scan order is possible
+    data_order = :normal
+
+    FormatParser::Image.new(
+      format: :bmp,
+      width_px: width,
+      height_px: height,
+      color_mode: :rgb,
+      intrinsics: {
+        data_order: data_order,
+        bits_per_pixel: bit_depth
+      }
+    )
+  end
+
+  def parse_modern_header(dib_header)
+    _header_size, width, height, _planes, bits_per_pixel,
     _compression_method, _image_size, horizontal_res,
     vertical_res, _n_colors, _i_colors = dib_header.unpack('Vl<2v2V2l<2V2')
 
     # There are cases where the height might by negative indicating the data
     # is ordered from top to bottom instead of bottom to top
-    # http://www.dragonwins.com/domains/getteched/bmp/bmpfileformat.htm#The%20Image%20Header
     data_order = height < 0 ? :inverse : :normal
 
     FormatParser::Image.new(
@@ -39,7 +66,8 @@ class FormatParser::BMPParser
       intrinsics: {
         vertical_resolution: vertical_res,
         horizontal_resolution: horizontal_res,
-        data_order: data_order
+        data_order: data_order,
+        bits_per_pixel: bits_per_pixel,
       }
     )
   end
