5aaa4ef59f098a7c7ad3be5a525f5762c8c28541
Fabio Perrella <fabio.perrella@gmail.com>
2020-09-07 12:32:19 -0300

    Use ID3Tag.local_configuration to not overwrite the global one

--- lib/parsers/mp3_parser.rb
+++ lib/parsers/mp3_parser.rb
@@ -1,11 +1,6 @@
 require 'ks'
 require 'id3tag'
 
-ID3Tag.configuration do |c|
-  c.string_encode_options = { invalid: :replace, undef: :replace }
-  c.source_encoding_fallback = Encoding::UTF_8
-end
-
 class FormatParser::MP3Parser
   include FormatParser::IOUtils
 
@@ -86,7 +81,7 @@ class FormatParser::MP3Parser
 
     first_frame = initial_frames.first
 
-    id3tags_hash = blend_id3_tags_into_hash(*tags)
+    id3tags_hash = with_id3tag_local_configs { blend_id3_tags_into_hash(*tags) }
 
     file_info = FormatParser::Audio.new(
       format: :mp3,
@@ -296,6 +291,15 @@ class FormatParser::MP3Parser
     attrs[:artist] = FormatParser.string_to_lossy_utf8(id3tags_hash[:artist]) unless id3tags_hash[:artist].to_s.empty?
 
     attrs
+  end
+
+  def with_id3tag_local_configs
+    ID3Tag.local_configuration do |c|
+      c.string_encode_options = { invalid: :replace, undef: :replace }
+      c.source_encoding_fallback = Encoding::UTF_8
+
+      yield
+    end
   end
 
   FormatParser.register_parser new, natures: :audio, formats: :mp3, priority: 99
