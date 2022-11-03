b5bdb73a0075eecde495b69e8efc310d679e557a
Kevin McGonigle <kevin.mcgonigle@wetransfer.com>
2022-11-03 17:36:03 +0000

    Add metadata test.

--- spec/parsers/moov_parser_spec.rb
+++ spec/parsers/moov_parser_spec.rb
@@ -87,6 +87,24 @@ describe FormatParser::MOOVParser do
     end
   end
 
+  it 'parses a CR3 file and provides the necessary metadata' do
+    cr3_path = fixtures_dir + '/MOOV/CR3/Canon EOS R10 (RAW).CR3'
+
+    result = subject.call(File.open(cr3_path, 'rb'))
+    expect(result.nature).to eq(:image)
+    expect(result.width_px).to eq(6000)
+    expect(result.height_px).to eq(4000)
+    expect(result.orientation).to eq(:top_left)
+    expect(result.display_width_px).to eq(6000)
+    expect(result.display_height_px).to eq(4000)
+    expect(result.content_type).to eq('image/x-canon-cr3')
+    expect(result.intrinsics).not_to be_nil
+    expect(result.intrinsics[:atom_tree]).not_to be_nil
+    expect(result.intrinsics[:exif]).not_to be_nil
+    expect(result.intrinsics[:exif][:image_length]).to eq(result.height_px)
+    expect(result.intrinsics[:exif][:image_width]).to eq(result.width_px)
+  end
+
   it 'parses an M4A file and provides the necessary metadata' do
     m4a_path = fixtures_dir + '/MOOV/M4A/fixture.m4a'
 
