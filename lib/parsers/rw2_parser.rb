6596d1a2b638b27df9aaa35fa2b6ae4213968f1f
Kevin McGonigle <kevin.mcgonigle@wetransfer.com>
2022-12-05 15:02:13 +0100

    [CORE-88] RW2 Support

---
+++ lib/parsers/rw2_parser.rb
@@ -0,0 +1,61 @@
+require_relative 'exif_parser'
+
+class FormatParser::RW2Parser
+  include FormatParser::IOUtils
+  include FormatParser::EXIFParser
+
+  def likely_match?(filename)
+    filename =~ /\.(rw2|raw|rwl)$/i
+  end
+
+  def call(io)
+    @buf = FormatParser::IOConstraint.new(io)
+
+    return unless matches_rw2_definition?
+
+    @buf.seek(0)
+    exif = exif_from_tiff_io(@buf)
+    return unless exif
+
+    # RW2 doesn't use the standard EXIF width and height tags (🤷🏻). We can compute them from the sensor
+    # top/bottom/left/right border tags. See https://exiftool.org/TagNames/PanasonicRaw.html for more.
+    w = sensor_border(exif, :right) - sensor_border(exif, :left)
+    h = sensor_border(exif, :bottom) - sensor_border(exif, :top)
+
+    FormatParser::Image.new(
+      format: :rw2,
+      width_px: w,
+      height_px: h,
+      display_width_px: exif.rotated? ? h : w,
+      display_height_px: exif.rotated? ? w : h,
+      orientation: exif.orientation_sym,
+      intrinsics: { exif: exif },
+      content_type: PANASONIC_RAW_MIMETYPE,
+    )
+  rescue EXIFR::MalformedTIFF
+    nil
+  end
+
+  private
+
+  PANASONIC_RAW_MIMETYPE = 'image/x-panasonic-raw'
+  RW2_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x18, 0x0, 0x0, 0x0].pack('C8')
+  RAW_RWL_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x08, 0x0, 0x0, 0x0].pack('C8')
+  MAGIC_BYTES = [RW2_MAGIC_BYTES, RAW_RWL_MAGIC_BYTES]
+  BORDER_TAG_IDS = {
+    top: 4,
+    left: 5,
+    bottom: 6,
+    right: 7
+  }
+
+  def matches_rw2_definition?
+    MAGIC_BYTES.include?(read_bytes(8))
+  end
+
+  def sensor_border(exif, border)
+    exif[0].raw_fields[BORDER_TAG_IDS[border]][0]
+  end
+
+  FormatParser.register_parser new, natures: [:image], formats: [:rw2]
+end
---
+++ spec/parsers/rw2_parser_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe FormatParser::RW2Parser do
+  context 'when likely_match? is called' do
+    %w[rw2 RW2 raw RAW rwl RWL].each do |extension|
+      context "with a file with a .#{extension} extension" do
+        it "should return true" do
+          expect(subject.likely_match?("foo.#{extension}")).to be_truthy
+        end
+      end
+    end
+
+    ['', 'foo', 'rw2', 'foorw2', 'foo.rw', 'foo.rw2.bar'].each do |filename|
+      context "with a file named #{filename}" do
+        it 'should return false' do
+          expect(subject.likely_match?("#{filename}")).to be_falsey
+        end
+      end
+    end
+  end
+
+  context 'when call is called' do
+    %w[rw2 raw rwl].map { |extension| Dir.glob(fixtures_dir + "/RW2/*.#{extension}") }.flatten.sort.each do |path|
+      it "should successfully parse #{path}" do
+        result = subject.call(File.open(path, 'rb'))
+
+        expect(result).not_to be_nil
+        expect(result.nature).to eq(:image)
+        expect(result.width_px).to be > 0
+        expect(result.height_px).to be > 0
+        expect(result.content_type).to eq('image/x-panasonic-raw')
+        expect(result.intrinsics).not_to be_nil
+      end
+    end
+
+    %w[ARW/RAW_SONY_A100.ARW NEF/RAW_NIKON_D1X.NEF TIFF/test.tif].each do |path|
+      it "should not parse #{path}" do
+        result = subject.call(File.open(fixtures_dir + path, 'rb'))
+        expect(result).to be_nil
+      end
+    end
+
+    it 'parses the necessary metadata from an RW2 file' do
+      file_path = fixtures_dir + '/RW2/Panasonic - DMC-G2 - 16_9.RW2'
+
+      result = subject.call(File.open(file_path, 'rb'))
+      expect(result.nature).to eq(:image)
+      expect(result.width_px).to eq(4000)
+      expect(result.height_px).to eq(2248)
+      expect(result.orientation).to eq(:top_left)
+      expect(result.display_width_px).to eq(4000)
+      expect(result.display_height_px).to eq(2248)
+      expect(result.content_type).to eq('image/x-panasonic-raw')
+      expect(result.intrinsics).not_to be_nil
+      expect(result.intrinsics[:exif]).not_to be_nil
+    end
+  end
+end
