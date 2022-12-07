c32ea79a8b9cc987301bd1ea5de357ed7ad17e01
Kevin McGonigle <kevin.mcgonigle@wetransfer.com>
2022-12-07 20:00:52 +0100

    PR suggestions.

--- lib/parsers/rw2_parser.rb
+++ lib/parsers/rw2_parser.rb
@@ -4,8 +4,19 @@ class FormatParser::RW2Parser
   include FormatParser::IOUtils
   include FormatParser::EXIFParser
 
+  PANASONIC_RAW_MIMETYPE = 'image/x-panasonic-raw'
+  RW2_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x18, 0x0, 0x0, 0x0].pack('C8')
+  RAW_RWL_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x08, 0x0, 0x0, 0x0].pack('C8')
+  MAGIC_BYTES = [RW2_MAGIC_BYTES, RAW_RWL_MAGIC_BYTES]
+  BORDER_TAG_IDS = {
+    top: 4,
+    left: 5,
+    bottom: 6,
+    right: 7
+  }
+
   def likely_match?(filename)
-    filename =~ /\.(rw2|raw|rwl)$/i
+    /\.(rw2|raw|rwl)$/i.match?(filename)
   end
 
   def call(io)
@@ -19,8 +30,13 @@ class FormatParser::RW2Parser
 
     # RW2 doesn't use the standard EXIF width and height tags (🤷🏻). We can compute them from the sensor
     # top/bottom/left/right border tags. See https://exiftool.org/TagNames/PanasonicRaw.html for more.
-    w = sensor_border(exif, :right) - sensor_border(exif, :left)
-    h = sensor_border(exif, :bottom) - sensor_border(exif, :top)
+    left_sensor_border = sensor_border(exif, :left)
+    right_sensor_border = sensor_border(exif, :right)
+    w = left_sensor_border && right_sensor_border && right_sensor_border - left_sensor_border
+
+    top_sensor_border = sensor_border(exif, :top)
+    bottom_sensor_border = sensor_border(exif, :bottom)
+    h = top_sensor_border && bottom_sensor_border && bottom_sensor_border - top_sensor_border
 
     FormatParser::Image.new(
       format: :rw2,
@@ -38,23 +54,12 @@ class FormatParser::RW2Parser
 
   private
 
-  PANASONIC_RAW_MIMETYPE = 'image/x-panasonic-raw'
-  RW2_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x18, 0x0, 0x0, 0x0].pack('C8')
-  RAW_RWL_MAGIC_BYTES = [0x49, 0x49, 0x55, 0x0, 0x08, 0x0, 0x0, 0x0].pack('C8')
-  MAGIC_BYTES = [RW2_MAGIC_BYTES, RAW_RWL_MAGIC_BYTES]
-  BORDER_TAG_IDS = {
-    top: 4,
-    left: 5,
-    bottom: 6,
-    right: 7
-  }
-
   def matches_rw2_definition?
     MAGIC_BYTES.include?(read_bytes(8))
   end
 
   def sensor_border(exif, border)
-    exif[0].raw_fields[BORDER_TAG_IDS[border]][0]
+    exif[0]&.raw_fields&.[](BORDER_TAG_IDS[border])&.[](0)
   end
 
   FormatParser.register_parser new, natures: [:image], formats: [:rw2]
--- spec/parsers/rw2_parser_spec.rb
+++ spec/parsers/rw2_parser_spec.rb
@@ -1,27 +1,27 @@
 require 'spec_helper'
 
 describe FormatParser::RW2Parser do
-  context 'when likely_match? is called' do
+  describe '#likely_match?' do
     %w[rw2 RW2 raw RAW rwl RWL].each do |extension|
       context "with a file with a .#{extension} extension" do
-        it "should return true" do
-          expect(subject.likely_match?("foo.#{extension}")).to be_truthy
+        it "returns true" do
+          expect(subject.likely_match?("foo.#{extension}")).to be(true)
         end
       end
     end
 
     ['', 'foo', 'rw2', 'foorw2', 'foo.rw', 'foo.rw2.bar'].each do |filename|
       context "with a file named #{filename}" do
-        it 'should return false' do
-          expect(subject.likely_match?(filename)).to be_falsey
+        it 'returns false' do
+          expect(subject.likely_match?(filename)).to be(false)
         end
       end
     end
   end
 
-  context 'when call is called' do
+  describe '#call' do
     Dir.glob(fixtures_dir + '/RW2/*.*').sort.each do |path|
-      it "should successfully parse #{path}" do
+      it "successfully parses #{path}" do
         result = subject.call(File.open(path, 'rb'))
 
         expect(result).not_to be_nil
@@ -34,13 +34,13 @@ describe FormatParser::RW2Parser do
     end
 
     %w[ARW/RAW_SONY_A100.ARW NEF/RAW_NIKON_D1X.NEF TIFF/test.tif].each do |path|
-      it "should not parse #{path}" do
+      it "does not parse #{path}" do
         result = subject.call(File.open(fixtures_dir + path, 'rb'))
         expect(result).to be_nil
       end
     end
 
-    it 'parses the necessary metadata from an RW2 file' do
+    it 'parses metadata correctly' do
       file_path = fixtures_dir + '/RW2/Panasonic - DMC-G2 - 16_9.RW2'
 
       result = subject.call(File.open(file_path, 'rb'))
